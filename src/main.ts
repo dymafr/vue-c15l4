e7bd01915e2d36e5625265dd9c7225c42663a60f
dymafr <erwan1309@hotmail.com>
2022-03-26 13:52:33 +0100

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { vFocus } from './shared/directives/vFocus';
+import { examplePlugin } from './shared/plugins/examplePlugin';
 
 const app = createApp(App);
 
 app.directive('focus', vFocus);
+
+app.use(examplePlugin);
 
 app.mount('#app');
