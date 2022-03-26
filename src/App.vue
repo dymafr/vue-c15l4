4b3a3b19d5043ce3bd31c79ca20e5369e014fdbe
dymafr <erwan1309@hotmail.com>
2022-03-26 14:03:00 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>{{ $greeting('Jean') }}</h1>
-  <input v-focus type="text" />
+  <input v-focus="{ firstFocus: true, color: 'green' }" type="text" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject } from 'vue';
+import { symGreeting } from './shared/plugins/examplePlugin';
+const greeting = inject(symGreeting);
+console.log(greeting('Paul'));
+</script>
 
 <style lang="scss">
 @import './assets/scss/base.scss';
